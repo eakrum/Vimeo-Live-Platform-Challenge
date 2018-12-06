86541eade8686dbbd2a9daf960b8462e13314abd
eawwal <eawwal@stevens.edu>
2018-12-05 20:27:09 -0500

    notes and start script

--- main.js
+++ main.js
@@ -2,22 +2,31 @@ const fs = require("fs");
 
 let { writeFile, videoValid, validator } = require("./vimeo");
 
-let csvFile = process.argv[2];
+let csvFile = process.argv[2]; //get the appropriate csv file by entering into command line.
 
+
+/** Callback function that is needed to run fs.readFile **/
 function dataRetriever(err, data) {
   if (err) throw err;
 
+  //cuts out the first line of column names, removes spaces, and splits by line
   const totalVideos = data
     .trim(" ")
     .split(`\n`)
     .slice(1);
 
-  const validVideos = validator(totalVideos);
+  const validVideos = validator(totalVideos); //run the main validation function found in "vimeo.js"
+
+
+
+  /*Did not need to do this part according to problem statement but figured some quick totals in the console would be nice
+  for some quick analysis and testing. */
   console.log(`Total videos: ${totalVideos.length}`);
   console.log(`Valid videos: ${validVideos.valid.length}`);
   console.log(`Invalid videos: ${validVideos.invalid.length}`);
 }
 
+//read the file, use above callback to do manipulation.
 fs.readFile(csvFile, "utf-8", dataRetriever);
 
 module.exports = {
--- vimeo.js
+++ vimeo.js
@@ -1,19 +1,20 @@
 const fs = require("fs");
 
-const csvFile = "./test.csv";
-
+/***  FUNCTION THAT WRITES DATA TO RESPECTIVE FILE  ***/
 function writeFile(fileName, filteredData) {
   fs.appendFile(fileName, filteredData, err => {
     if (err) throw err;
   });
 }
-
+/*** FUNCTION THAT CHECKS IF VIDEOS ARE VALID OR INVALID ***/
 function videoValid(videoData) {
-  //validate all of the data received from the CSV file based on requirements outlined by the problem given
-  const validated = {};
+  const validated = {}; //object that will have key value pair of video id and valid or invalid videos. i.e {200111: "valid"}
 
+  //validate all of the data received from the CSV file based on requirements outlined by the problem given
   videoData.reduce((acc, rowString) => {
     const row = rowString.split(",");
+
+    //if clause that will check for validity (valid), if valid object key value pair will be assigned
     if (
       row[1].length < 30 &&
       row[2] === "anybody" &&
@@ -22,32 +23,45 @@ function videoValid(videoData) {
     ) {
       const validValue = parseInt(row[0]);
       validated[validValue] = "valid";
+
+      //else clause that will check for validity (invalid), if invalid object key value pair will be assigned
     } else {
       const invalidValue = parseInt(row[0]);
       validated[invalidValue] = "invalid";
     }
   }, []);
 
+  //returns the object of key value pairs indicating if valid or invalid
   return validated;
 }
 
-/** MAIN VALIDATOR FUNCTION  **/
+/**************************************************************************************************************************
+*                                          MAIN VALIDATOR FUNCTION
+This function calls the "videoValid" and "writeFile" helper functions to verify and append files. Although this function combination 
+goes through an extra pass, it has an added functionality of returning an object of two arrays, one of invalid videos and one 
+of valid videos. Doing so can add more features or data manipulation and is easily available to use at anybody else's discretion.
+*
+******************************************************************************************************************************/
 function validator(csvData) {
-  const valid = [];
-  const invalid = [];
-  const checkedVideos = videoValid(csvData);
+  const valid = []; //an array to hold valid video ID's.
+  const invalid = []; //an array to hold invalid video ID's
+  const checkedVideos = videoValid(csvData); //this will be an object of all valid and invalid videos.
+
+  //traverses through the object's key value pairs, pushes each video ID into respective array.
   Object.entries(checkedVideos).forEach(([key, value]) => {
-    if (value == "valid") {
+    if (value === "valid") {
       valid.push(key);
-    } else if (value == "invalid") {
+    } else if (value === "invalid") {
       invalid.push(key);
     }
   });
 
+  //write the respective files with data stored
   writeFile("./data/invalid.csv", invalid);
   writeFile("./data/valid.csv", valid);
 
-  //I didn't need to do this part but figured it would be a nice added feature to just see some quick totals in the console.
+  //I didn't need to do this part but figured it would be a nice added feature for future functionality and
+  //to just see some quick totals in the console.
   return {
     valid: valid,
     invalid: invalid
